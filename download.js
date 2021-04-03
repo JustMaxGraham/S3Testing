1f5ecc59762a7ea8f887edc27ab0bd1567e7ae9c
MaxHuw <m.huw.graham@gmail.com>
2021-04-02 22:27:55 -0400

    Trigger lambda after downloading file

--- download.js
+++ download.js
@@ -9,27 +9,44 @@ const ID = process.env.AWS_ID;
 const SECRET = process.env.AWS_SECRET;
 const BUCKET_NAME = process.env.AWS_BUCKET;
 
-const S3 = new AWS.S3({
+AWS.config.update({
+    region: process.env.AWS_REGION,
     accessKeyId: ID,
     secretAccessKey: SECRET
+});
+
+const S3 = new AWS.S3({
 })
+
+const lambda = new AWS.Lambda({}); 
 
 const downloadFile = (file) => {
 
     const fileName = path.basename(file)
 
-    const params = {
+    const S3Params = {
         Bucket: BUCKET_NAME, 
         Key: fileName,
     };
 
-    S3.getObject(params, function(err, data) {
+    const lambdaParams = {
+        FunctionName: 'hellWorldDownloaded'
+    }
+
+    S3.getObject(S3Params, function(err, data) {
         if (err) {
             throw err;
         }
 
         fs.writeFileSync(`./${fileName}`, data.Body)
         console.log(`File Downloaded Succcessfully!`);
+
+        lambda.invoke(lambdaParams, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log('Lambda Invoked!')
+        })
     });
 };
 
